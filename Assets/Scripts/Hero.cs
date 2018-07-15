92a1c4bdb52d38d6f286d213279ac99fdaab2b5e
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-07-15 16:21:11 +0300

    Create Hero and his control scripts

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class Hero : Liveble, IAbilityCaster
+{
+    public HeroClassAbilities ClassAbilities;
+    public GameObject Weapon;
+
+    private HeroMoveControl _heroMoveControl;
+    private Quaternion _startRotation;
+    
+    void Start()
+    {
+        _startRotation = transform.rotation;
+        _heroMoveControl = GetComponent<HeroMoveControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAttackAbilityButton();
+    }
+
+    private void CheckAttackAbilityButton()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            OnAttackAbilityUsed();
+        }
+    }
+
+    private void OnAttackAbilityUsed()
+    {
+        ClassAbilities.AttackAbility.Use();
+    }
+
+    public void SetMoveConfig(MoveConfig moveConfig)
+    {
+        _heroMoveControl.MoveConfig = moveConfig;
+    }
+
+    public void InitClassAbilities(HeroClassAbilities classAbilities)
+    {
+        ClassAbilities = classAbilities;
+        foreach (var heroAbility in ClassAbilities.GetAbilityList())
+        {
+            heroAbility.SetAbilityCaster(this);
+        }
+    }
+
+    public void InitWeapon(GameObject weapon)
+    {
+        // Set start rotation and save current
+        var currentRotation = transform.rotation;
+        transform.rotation = _startRotation;
+        
+        // Create weapon and set transform for him
+        Weapon = Instantiate(weapon);
+        Weapon.transform.parent = transform;
+        Weapon.transform.position = new Vector2(transform.position.x + weapon.transform.position.x,
+            transform.position.y + weapon.transform.position.y);
+        
+        // Return curent rotation
+        transform.rotation = currentRotation;
+    }
+}
---
+++ Assets/Scripts/HeroMoveControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HeroMoveControl : MonoBehaviour
+{
+    public MoveConfig MoveConfig;
+
+    private Vector3 _direction;
+    private Rigidbody2D _body;
+    private static readonly float DirectionSpeedBound = Mathf.Sqrt(2) / 2;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        var mouseAngle = MouseUtils.GetMouseAndTargetAngle(transform.position);
+        SetMovementParams();
+        LookAtCursor(mouseAngle);
+    }
+
+    private void FixedUpdate()
+    {
+        var currentSpeed = MoveConfig.Speed * Time.deltaTime;
+
+        if (Mathf.Abs(_direction.x) > 0 && Mathf.Abs(_direction.y) > 0)
+        {
+            currentSpeed *= DirectionSpeedBound;
+        }
+        _body.velocity = _direction * currentSpeed;
+    }
+
+    private void LookAtCursor(float mouseAngle)
+    {
+        transform.rotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
+    }
+
+    private void SetMovementParams()
+    {
+        _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+}
