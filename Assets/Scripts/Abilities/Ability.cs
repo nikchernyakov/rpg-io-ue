03382f1217f164cd503d16a8d42c37ebc3c8efdb
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-07-15 16:20:28 +0300

    Create abilities script

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public abstract class Ability<T> : MonoBehaviour
+    where T : IAbilityCaster
+{
+    public float AbilityCooldown;
+
+    protected T AbilityCaster;
+    private bool _inCooldown = false;
+    private float _currentCooldown;
+
+    public void SetAbilityCaster(T abilityCaster)
+    {
+        AbilityCaster = abilityCaster;
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_inCooldown)
+        {
+            CheckCooldown();
+        }
+    }
+
+    private void CheckCooldown()
+    {
+        if (_currentCooldown > 0)
+        {
+            _currentCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            _inCooldown = false;
+            _currentCooldown = 0;
+        }
+    }
+
+    public bool IsInCooldown()
+    {
+        return _inCooldown;
+    }
+
+    public void Use()
+    {
+        if (_inCooldown)
+        {
+            Debug.Log("Ability in cooldown");
+            return;
+        }
+        
+        AbilityAction();
+        _inCooldown = true;
+        _currentCooldown = AbilityCooldown;
+    }
+
+    protected abstract void AbilityAction();
+}
---
+++ Assets/Scripts/Abilities/HeroClassAbilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroClassAbilities : MonoBehaviour
+{
+    public HeroAbility AttackAbility;
+
+    public IEnumerable<HeroAbility> GetAbilityList()
+    {
+        return new List<HeroAbility>() {AttackAbility};
+    } 
+}
