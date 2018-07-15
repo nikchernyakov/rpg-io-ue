6828a9096b6e22a0536825227c4575c9cacc6398
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-07-15 16:19:39 +0300

    Add MouseUtils Script

---
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class MouseUtils {
+    
+    public static float GetMouseAndTargetAngle(Vector3 targetPosition)
+    {
+        var lookPos = GetMousePosition() - targetPosition;
+        return Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+    }
+
+    public static Vector3 GetMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.transform.position.z));
+    }
+}
