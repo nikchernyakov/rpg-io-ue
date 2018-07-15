43a850bd2925424eac71fd10f24cafe2830dc4be
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-07-15 16:19:57 +0300

    Add utils for set class

---
+++ Assets/Scripts/Utils/ClassSetter.cs
@@ -0,0 +1,11 @@
+﻿
+public class ClassSetter
+{
+    public static void SetClassToHero(Hero hero, HeroClassConfig heroClassConfig)
+    {
+        hero.SetMoveConfig(heroClassConfig.MoveConfig);
+        hero.HpConfig = heroClassConfig.HpConfig;
+        hero.InitClassAbilities(heroClassConfig.ClassAbilities);
+        hero.InitWeapon(heroClassConfig.Weapon);
+    }
+}
